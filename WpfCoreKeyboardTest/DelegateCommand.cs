4a256718143af28e765ad69f6cd8004ab0da4a3a
mrxx99 <mrxx99@googlemail.com>
2019-07-11 21:28:00 +0200

    Add project files.

---
+++ WpfCoreKeyboardTest/DelegateCommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace WpfCoreKeyboardTest
+{
+    public class DelegateCommand : ICommand
+    {
+        private Action<object> execute;
+        private Func<object, bool> canExecute;
+
+        public DelegateCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return execute != null && (canExecute == null || canExecute(parameter));
+        }
+
+        public void Execute(object parameter)
+        {
+            execute?.Invoke(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged = delegate { };
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, new EventArgs());
+        }
+    }
+}
